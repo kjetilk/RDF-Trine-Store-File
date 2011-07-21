183fd4d3db17c051bd8b79cd5d29a40362382885
Kjetil Kjernsmo <kjetil@kjernsmo.net>
2011-07-21 16:34:07 +0200

    first commit

---
+++ lib/RDF/Trine/Store/File.pm
@@ -0,0 +1,111 @@
+package RDF::Trine::Store::File;
+
+use 5.006;
+use strict;
+use warnings;
+
+=head1 NAME
+
+RDF::Trine::Store::File - The great new RDF::Trine::Store::File!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use RDF::Trine::Store::File;
+
+    my $foo = RDF::Trine::Store::File->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 SUBROUTINES/METHODS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+Kjetil Kjernsmo, C<< <kjetilk at cpan.org> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-rdf-trine-store-file at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=RDF-Trine-Store-File>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc RDF::Trine::Store::File
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker (report bugs here)
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=RDF-Trine-Store-File>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/RDF-Trine-Store-File>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/RDF-Trine-Store-File>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/RDF-Trine-Store-File/>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2011 Kjetil Kjernsmo.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of either: the GNU General Public License as published
+by the Free Software Foundation; or the Artistic License.
+
+See http://dev.perl.org/licenses/ for more information.
+
+
+=cut
+
+1; # End of RDF::Trine::Store::File
---
+++ t/00-load.t
@@ -0,0 +1,9 @@
+#!perl -T
+
+use Test::More tests => 1;
+
+BEGIN {
+    use_ok( 'RDF::Trine::Store::File' ) || print "Bail out!\n";
+}
+
+diag( "Testing RDF::Trine::Store::File $RDF::Trine::Store::File::VERSION, Perl $], $^X" );
