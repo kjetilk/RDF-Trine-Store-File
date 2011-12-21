0f0ca0a0854673f9571e56d2c68b7f42385e15a5
Kjetil Kjernsmo <kjetil@kjernsmo.net>
2011-12-21 14:06:16 +0100

    Bump version

--- lib/RDF/Trine/Store/File.pm
+++ lib/RDF/Trine/Store/File.pm
@@ -22,11 +22,11 @@ RDF::Trine::Store::File - Using a file with N-Triples as triplestore
 
 =head1 VERSION
 
-Version 0.03_3
+Version 0.1
 
 =cut
 
-our $VERSION = '0.03_3';
+our $VERSION = '0.1';
 
 
 =head1 SYNOPSIS
