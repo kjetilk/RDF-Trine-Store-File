c2a2707be21295be1f9b027948c99036152d28e9
Kjetil Kjernsmo <kjetil@kjernsmo.net>
2014-11-19 14:37:32 +0100

    bump copyright year

--- lib/RDF/Trine/Store/File.pm
+++ lib/RDF/Trine/Store/File.pm
@@ -393,7 +393,7 @@ L<http://search.cpan.org/dist/RDF-Trine-Store-File/>
 
 =head1 LICENSE AND COPYRIGHT
 
-Copyright 2011-2012 Kjetil Kjernsmo.
+Copyright 2011-2012,2014 Kjetil Kjernsmo.
 
 This program is free software; you can redistribute it and/or modify it
 under the terms of either: the GNU General Public License as published
